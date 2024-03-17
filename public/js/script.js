2c43f16697b3e36cd34490a2d48bcecd9909dbdf
Osama Tamer Rizk <105207486+osama4713@users.noreply.github.com>
2024-03-18 00:49:12 +0200

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,34 +1,40 @@
-var form = document.getElementById("form1")
-var addr = document.getElementById("address")
+
+var form = document.getElementById("form1");
+var addr = document.getElementById("address");
 
 
-var loca = document.getElementById("location")
-var forc = document.getElementById("forecast")
-var err = document.getElementById("error")
+var loca = document.getElementById("location");
+var forc = document.getElementById("forecast");
+var err = document.getElementById("error");
 
-form.addEventListener("submit" , (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault()
+    //console.log(addr.value)
     weatherFunction()
     form.reset()
 })
+
 let weatherFunction = async () => {
-    try{
+    try {
         const Address = addr.value
-        const res = await fetch ("http://localhost:3000/weather?address="+Address)
+        const res = await fetch("http://localhost:3000/weather?address=" + Address)
         const data = await res.json()
         console.log(data)
 
-        if(data.error){
+        if (data.error) {
             err.innerText = data.error
             loca.innerText = ''
             forc.innerText = ''
-        }else{
-            loca.innerText = data.locaion
-            forc.innerText = data.forcast
+        } else {
+            loca.innerText = data.Address
+
+            setTimeout(() => {
+                forc.innerText = data.forcast
+            }, 2000)
             err.innerText = ''
         }
 
-    }catch(e){
+    } catch (e) {
         console.log(e)
     }
 }
